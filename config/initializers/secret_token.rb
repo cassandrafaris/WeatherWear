6748fd746a794a0d0e1de5e887e8f66fca02cf8c
Cassandra Faris <cassandra.faris@gmail.com>
2013-02-24 17:20:00 -0500

    Setup initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeatherWear::Application.config.secret_token = '0b45e3e7f97515ee122fddd2d7bc849ca2d4f6b3ecd79c50ba91ed7e3c16ab580a62ff087af90edf5ba6d0411904d8d61c0ab1416cb2696b531af0ac41982932'
